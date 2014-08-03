2b6e8147158713a06a38f89e623834541b915f62
ramso <ramso@marte.ramso.net>
2014-08-03 11:21:57 +0200

    Topic jaxb

---
+++ dita/dita.topic/src/net/ramso/dita/topic/package-info.java
@@ -0,0 +1,8 @@
+//@XmlSchema(
+//
+//namespace = "http://docs.oasis-open.org/dita/v1.2/", elementFormDefault = XmlNsForm.QUALIFIED, attributeFormDefault = XmlNsForm.UNQUALIFIED)
+package net.ramso.dita.topic;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
+
